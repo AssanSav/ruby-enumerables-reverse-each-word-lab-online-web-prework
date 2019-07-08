7d8f4ad12d734bf2b3f0155733f945e1d66516e5
assane savadogo <assane.savadogo81@gmail.com>
2019-07-08 21:10:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,8 @@
 def reverse_each_word(sentence)
-  #sentence = {"Hello there, and how are you?"}
- string = sentence.split(" ")
+  string = sentence.split(" ")
   reverse_word = []
   string.each do |word|
-   reverse_word.push(word.reverse)
- end
- return reverse_word.join(" ")
+    reverse_word << word.reverse 
+  end 
+  return reverse_word.join(" ")
 end
-#expect(reverse_each_word(sentence1)).to eq("olleH ,ereht dna woh era ?uoy")
