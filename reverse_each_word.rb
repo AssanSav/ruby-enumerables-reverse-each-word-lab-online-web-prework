01c5f12bf3b9b8afef6685a265bf54699bc310a8
assane savadogo <assane.savadogo81@gmail.com>
2019-07-08 21:18:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -11,6 +11,7 @@ def reverse_each_word(sentence)
   string = sentence.split(" ")
   reverse_word = []
   string.each do |word| 
-    
+    reverse_word.push(word.reverse)
   end
+  return reverse_word.join(" ")
 end
